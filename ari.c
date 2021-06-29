23ecb9700b40d9defcc207c0be253f0041d12100
mo-b-a <84846038+mo-b-a@users.noreply.github.com>
2021-06-29 22:22:57 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -34,29 +34,29 @@ string ari(string x)
              break;
     case 2: return "First/Second Grade";
              break;
-    case 3: return"Thried Grade";
+    case 3: return "Third Grade";
             break;
-    case 4: return"Fourth Grade";
+    case 4: return "Fourth Grade";
     	    break;
-    case 5:return="Fifth Grade";
+    case 5:return "Fifth Grade";
            break;
-    case 6:return="Sixth Grade";
+    case 6:return "Sixth Grade";
     	    break;
-    case 7: return="Seventh Grade";
+    case 7: return "Seventh Grade";
     	    break;
-    case 8: return="Eighth Grade";
+    case 8: return "Eighth Grade";
     	  break;
-    case 9:return="Ninth Grade";
+    case 9:return "Ninth Grade";
     	    break;
-    case 10: return="Tenth Grade";
+    case 10: return "Tenth Grade";
     	     break;
-    case 11: return="Eleventh Grade";
+    case 11: return "Eleventh Grade";
     	     break;
-    case 12: return="Twelfth Grade";
+    case 12: return "Twelfth grade";
              break;
-    case 13: return="College Student";
+    case 13: return "College student";
              break;
-    case 14: return="Professor"
+    case 14: return "Professor"
     		break;
     		
    };
